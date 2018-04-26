4ffaf6c88570bae7bab3f0ab3b3c164511c3f9f1
santiagoOrtiz0330 <36429742+santiagoOrtiz0330@users.noreply.github.com>
2018-04-25 21:24:09 -0500

    Add files via upload
    Parcial Administración Base de datos - Santiago Ortíz Pineda, Grupo Lunes - Miercoles / 8pm -10pm

---
+++ santiago_ortiz_pineda.sql
@@ -0,0 +1,47 @@
+--------------------------------------------------------
+-- Archivo creado  - miércoles-abril-25-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Function FIBONACCI
+--------------------------------------------------------
+
+  CREATE OR REPLACE FUNCTION "EJERCICIOS"."FIBONACCI" (n IN NUMBER) RETURN NUMBER IS
+  first number:=0;
+  second number:=1;
+  third number;
+  i number;
+begin
+
+    for i in 2..n
+    loop
+        third:=first+second;
+        first:=second;
+        second:=third;
+    end loop;
+    Return third;
+end;
+--------------------------------------------------------
+--  DDL for Function FUNCTION_1
+--------------------------------------------------------
+
+  CREATE OR REPLACE FUNCTION "EJERCICIOS"."FUNCTION_1" (a IN NUMBER, b IN NUMBER, c IN NUMBER) RETURN VARCHAR IS
+  x VARCHAR(255);
+BEGIN
+    IF (b >= c) THEN
+      x := a || '/' || a * b || '/' || 'andres';
+    ELSE
+      x := a || '/' || a * b || '/' || 'martinez';
+    END IF;
+    Return x;
+END;
+--------------------------------------------------------
+--  DDL for Function FUNCTION_2
+--------------------------------------------------------
+
+  CREATE OR REPLACE FUNCTION "EJERCICIOS"."FUNCTION_2" (n IN NUMBER) RETURN NUMBER IS
+  a number:=0;
+BEGIN
+   
+    a := (2 * Fibonacci(n + 1)) - 1;
+    Return a;
+END;
